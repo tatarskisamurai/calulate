84cd04e2e1551ec6d0816e7da36358290ded0639
tatarskisamurai <160776533+tatarskisamurai@users.noreply.github.com>
2024-02-27 22:43:42 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
 let result = '';
-const nul = document.querySelector('div.nul');
 const num = document.querySelectorAll('div.num');
-const plus = document.querySelector('div.plus');
-const proizv = document.querySelector('div.proizv');
-const chast = document.querySelector('div.chast');
-const proc = document.querySelector('div.proc');
 const equal = document.querySelector('div.equal');
 const element = document.querySelector('div.result');
 const del = document.querySelector('div.del');
+
 equal.addEventListener('click', function () {
    if(result=='0'){
       result=''
@@ -33,48 +29,18 @@ equal.addEventListener('click', function () {
       element.textContent = +(array[0])%(+array[1]);
    }
 });
+
 num.forEach(item => {
    item.addEventListener('click', () => {
       if(result=='0'){
          result=''
-         console.log(0);
-      }else if(result==''){
-         result=''
-         console.log(1);
       }
       result += item.textContent;
       element.textContent = result;
    })
 })
-plus.addEventListener('click', function () {
-   if(result=='0'){
-      result=''
-   }
-   result += '+';
-   element.textContent = result;
- });
+
 del.addEventListener('click', function () {
    result = '0';
    element.textContent = result;
  });
-proizv.addEventListener('click', function () {
-   if(result=='0'){
-      result=''
-   }
-   result += '*';
-   element.textContent = result;
- });
-chast.addEventListener('click', function () {
-   if(result=='0'){
-      result=''
-   }
-   result += '/';
-   element.textContent = result;
-});
-proc.addEventListener('click', function () {
-   if(result=='0'){
-      result=''
-   }
-   result += '%';
-   element.textContent = result;
-});
