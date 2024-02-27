68e443890ba273670570230b84c2eab3056234b1
tatarskisamurai <160776533+tatarskisamurai@users.noreply.github.com>
2024-02-27 22:25:05 +0300

    Update main.js
    -общая функция для всех кнопок цифр
    -исправлено значение result

--- main.js
+++ main.js
@@ -1,14 +1,6 @@
-let result = '1';
+let result = '';
 const nul = document.querySelector('div.nul');
-const btn1 = document.querySelector('div.btn1');
-const btn2 = document.querySelector('div.btn2');
-const btn3 = document.querySelector('div.btn3');
-const btn4 = document.querySelector('div.btn4');
-const btn5 = document.querySelector('div.btn5');
-const btn6 = document.querySelector('div.btn6');
-const btn7 = document.querySelector('div.btn7');
-const btn8 = document.querySelector('div.btn8');
-const btn9 = document.querySelector('div.btn9');
+const num = document.querySelectorAll('div.num');
 const plus = document.querySelector('div.plus');
 const proizv = document.querySelector('div.proizv');
 const chast = document.querySelector('div.chast');
@@ -42,77 +34,19 @@ equal.addEventListener('click', function () {
       element.textContent = +(array[0])%(+array[1]);
    }
 });
-
-btn1.addEventListener('click', function () {
-   if(result=='0'){
-      result=''
-   }
-   result += '1';
-   element.textContent = result;
- });
-btn2.addEventListener('click', function () {
-   if(result=='0'){
-      result=''
-   }
-   result += '2';
-   element.textContent = result;
- });
-
-btn3.addEventListener('click', function () {
-   if(result=='0'){
-      result=''
-   }
-   result += '3';
-   element.textContent = result;
- });
-
-btn4.addEventListener('click', function () {
-   if(result=='0'){
-      result=''
-   }
-   result += '4';
-   element.textContent = result;
- });
-
-btn5.addEventListener('click', function () {
-   if(result=='0'){
-      result=''
-   }
-   result += '5';
-   element.textContent = result;
- });
-
-btn6.addEventListener('click', function () {
-   if(result=='0'){
-      result=''
-   }
-   result += '6';
-   element.textContent = result;
- });
-
-btn7.addEventListener('click', function () {
-   if(result=='0'){
-      result=''
-   }
-   result += '7';
-   element.textContent = result;
- });
-
-btn8.addEventListener('click', function () {
-   if(result=='0'){
-      result=''
-   }
-   result += '8';
-   element.textContent = result;
- });
-
-btn9.addEventListener('click', function () {
-   if(result=='0'){
-      result=''
-   }
-   result += '9';
-   element.textContent = result;
- });
+num.forEach(item => {
+   item.addEventListener('click', () => {
+      if(result=='0'){
+         result=''
+         console.log(0);
+      }else if(result==''){
+         result=''
+         console.log(1);
+      }
+      result += item.textContent;
+      element.textContent = result;
+   })
+})
 
 plus.addEventListener('click', function () {
    if(result=='0'){
@@ -146,7 +80,7 @@ chast.addEventListener('click', function () {
 nul.addEventListener('click', function () {
    result += '0';
    element.textContent = result;
- });
+});
 
 proc.addEventListener('click', function () {
    if(result=='0'){
